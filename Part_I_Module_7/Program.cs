c98e3596ac9d3f664867fadad6046c4e5896a1ea
Сергей Чекалин <schekalin@mail.ru>
2022-04-29 23:07:35 +0300

    Итоговое задание.

--- Part_I_Module_7/Program.cs
+++ Part_I_Module_7/Program.cs
@@ -2,66 +2,100 @@
 
 namespace Part_I_Module_7
 {
-    abstract class Delivery
+    class Program
     {
-        public string Address;
-    }
-
-    abstract class Payment
-    {
-        public string Status;
-    }
-
-    class Prepayment : Payment
-    {
-        /* ... */
-    }
-
-    class PayOnDelivery : Payment
-    {
-        /* ... */
-    }
-
-    class PostponedPayment : Payment
-    {
-        /* ... */
-    }
-
-    class HomeDelivery : Delivery
-    {
-        /* ... */
-    }
-
-    class PickPointDelivery : Delivery
-    {
-        /* ... */
-    }
-
-    class ShopDelivery : Delivery
-    {
-        /* ... */
-    }
-
-    class Order<TDelivery,
-    TStruct, TPayment> where TDelivery : Delivery where TPayment : Payment
-    {
-        public TDelivery Delivery;
-
-        public TPayment Payment;
-
-        public int Number;
-
-        public string Description;
-
-        public void DisplayAddress()
+        static void Main()
         {
-            Console.WriteLine(Delivery.Address);
+
+            /* ... */
+
+        }
+        abstract class Delivery
+        {
+            public string Address;
         }
 
-        public void DisplayPaymentStatus()
+        abstract class Payment
         {
-            Console.WriteLine(Payment.Status);
+            public string Status;
+            public int Total_Sum;
+            public bool IsPaid;
+            public string TimeOfPayment;
+            public string NameOfBank;
+
+            public Payment(string status, int sum)
+            {
+                Status = status;
+                Total_Sum = sum;
+            }
         }
-        // ... Другие поля
+
+        class Prepayment : Payment
+        {
+            public Prepayment(string status, int sum, bool ispaid) : base(status, sum)
+            {
+                IsPaid = ispaid;
+            }
+            /* ... */
+        }
+
+        class PayOnDelivery : Payment
+        {
+            public PayOnDelivery(string status, int sum, string timeOfPayment) : base(status, sum)
+            {
+                TimeOfPayment = timeOfPayment;
+            }
+            /* ... */
+        }
+
+        class PostponedPayment : Payment
+        {
+            public PostponedPayment(string status, int sum, string nameOfBank) : base(status, sum)
+            {
+                NameOfBank = nameOfBank;
+            }
+            /* ... */
+        }
+
+        class HomeDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        class PickPointDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        class ShopDelivery : Delivery
+        {
+            /* ... */
+        }
+
+        class Order <TDelivery, TStruct, TPayment> where TDelivery : Delivery where TPayment : Payment
+        {
+            public TDelivery Delivery;
+
+            public int Number;
+
+            public string Description;
+
+            public void DisplayAddress()
+            {
+                Console.WriteLine(Delivery.Address);
+            }
+
+            // ... Другие поля
+
+            public TPayment Payment;
+            public void DisplayPaymentStatus()
+            {
+                Console.WriteLine(Payment.Status);
+                Console.WriteLine(Payment.NameOfBank ?? "Оплата от клиента");
+                Console.WriteLine(Payment.TimeOfPayment ?? "Оформлено по предоплате или отсрочке платежа");
+                Console.WriteLine(Payment.Total_Sum);
+            }
+
+        }
     }
 }
