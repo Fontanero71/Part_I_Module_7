9f37e8b419b8f1eaef3ef2e50c8cd8dbb9749a9d
Сергей Чекалин <schekalin@mail.ru>
2022-04-29 21:39:23 +0300

    Добавьте файлы проекта.

---
+++ Part_I_Module_7/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Part_I_Module_7
+{
+    abstract class Delivery
+    {
+        public string Address;
+    }
+
+    abstract class Payment
+    {
+        public string Status;
+    }
+
+    class Prepayment : Payment
+    {
+        /* ... */
+    }
+
+    class PayOnDelivery : Payment
+    {
+        /* ... */
+    }
+
+    class PostponedPayment : Payment
+    {
+        /* ... */
+    }
+
+    class HomeDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class PickPointDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class ShopDelivery : Delivery
+    {
+        /* ... */
+    }
+
+    class Order<TDelivery,
+    TStruct, TPayment> where TDelivery : Delivery where TPayment : Payment
+    {
+        public TDelivery Delivery;
+
+        public TPayment Payment;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+        public void DisplayPaymentStatus()
+        {
+            Console.WriteLine(Payment.Status);
+        }
+        // ... Другие поля
+    }
+}
